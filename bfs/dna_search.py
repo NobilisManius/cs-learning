58b41ec4db6d7746e58e0c14ac7f630b0fce6b37
NobilisManius <leiz0908@gmail.com>
2025-12-03 12:17:57 +0300

    feat(dna_search): init dna_search.py

---
+++ bfs/dna_search.py
@@ -0,0 +1,8 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+
+Gene = List[Codon]
