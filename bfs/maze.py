73f6dd26a96cfdbcbb23080612aa9c6b07301893
NobilisManius <leiz0908@gmail.com>
2025-12-03 13:04:56 +0300

    feat(maze): add Maze class

--- bfs/maze.py
+++ bfs/maze.py
@@ -15,3 +15,12 @@ class MazeLocation(NamedTuple):
     row: int
     col: int
 
+class Maze:
+    def __init__(self, rows: int = 10, cols: int = 10, sparseness: float = 0.1, start: MazeLocation = MazeLocation(0, 0), goal: MazeLocation = MazeLocation( 9, 9)) -> None:
+        self._rows: int = rows
+        self._cols: int = cols
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+        self._grid: List[List[Cell]] = [[Cell.Empty for _ in range(cols)] for _ in range(rows)]
+
+    
